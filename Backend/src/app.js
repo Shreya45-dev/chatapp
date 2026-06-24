e57bc4038089631edaa2d192f8b9e385d6f9785c
Shreya Pandey <shreyap2276@gmail.com>
2026-06-24 19:09:25 +0530

    in

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -12,7 +12,7 @@ const app = express();
 
 // 🔹 Middleware
 app.use(cors({
-  origin: "http://localhost:5173", // frontend URL
+  origin: "https://chatapp-1frontend.onrender.com", // frontend URL
   credentials: true
 }));
 app.use(express.json());
