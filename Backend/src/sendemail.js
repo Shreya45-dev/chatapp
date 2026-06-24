07291187618dd2b6847304e2cba5a7c07d748b35
Shreya Pandey <shreyap2276@gmail.com>
2026-06-25 00:25:39 +0530

    initial

--- Backend/src/sendemail.js
+++ Backend/src/sendemail.js
@@ -2,9 +2,10 @@ const nodemailer = require('nodemailer');
 
 const sendEmail = async (email, subject, message) => {
     const transporter = nodemailer.createTransport({
-        host: 'smtp.gmail.com',
-        port: 587,
-        secure: false,
+     //  host: 'smtp.gmail.com',
+      //  port: 587,
+      //  secure: false,
+      service: "gmail",
         auth: {
             user: process.env.PROJECT_EMAIL,
             pass: process.env.PASSWORD
